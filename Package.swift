660e383ff4c2dea6b7b8a7d28d1b7e75bdbfc164
agorabuilder <agorabuilder@agora.io>
2022-06-16 17:13:21 +0800

    4.0.0-beta.2

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AgoraRtcKit",
-            url: "https://download.agora.io/swiftpm_4/AgoraRtcEngine_iOS_Voice_Beta/4.0.0-beta.1/AgoraRtcKit.xcframework.zip",
-            checksum: "6587e89813c533bc85d419bd4ec769bc041ae76cc9d20adbf277d77f64979481"
+            url: "https://download.agora.io/swiftpm/AgoraRtcEngine_iOS_Voice_Beta/4.0.0-beta.2/AgoraRtcKit.xcframework.zip",
+            checksum: "48799c1ad73789530a90109418d11f987be64eed8a1ca3c4117403cb17be4353"
         ),
     ]
 )
